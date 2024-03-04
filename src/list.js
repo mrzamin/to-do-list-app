09366bbd02ca501fbe28652ba2ab4904b92ee1d3
mrzamin <142754418+mrzamin@users.noreply.github.com>
2024-03-04 11:55:49 -0500

    Create fcns for creating, editing, deleting, and getting tasks and lists

--- src/list.js
+++ src/list.js
@@ -1,11 +1,35 @@
+let lists = [];
+
 const List = (name, icon) => {
+  let tasks = [];
+  let completed = [];
   return {
     name,
     icon,
-    set setName(newName) {
-      this.name = newName;
-    },
+    tasks,
+    completed,
   };
 };
 
-let list1 = List("groceries", "icon");
+const createList = (chosenName, chosenIcon) => {
+  const newList = List(chosenName, chosenIcon);
+  lists.push(newList);
+};
+
+const editList = (listName, newListName, newIcon) => {
+  const selectedList = getList(listName);
+  selectedList.name = newListName;
+  selectedList.icon = newIcon;
+};
+
+const deleteList = (listName) => {
+  const selectedListIndex = getListIndex(listName);
+  lists.splice(selectedListIndex, 1);
+};
+
+const getList = (listName) => lists.find((list) => list.name === listName);
+
+const getListIndex = (listName) =>
+  lists.findIndex((list) => list.name === listName);
+
+export { createList, editList, deleteList, getList, getListIndex };
--- src/task.js
+++ src/task.js
@@ -1,3 +1,5 @@
+import * as list from "./list.js";
+
 const Task = (name, description, date, priority, list) => {
   return {
     name,
@@ -6,26 +8,32 @@ const Task = (name, description, date, priority, list) => {
     priority,
     list,
 
-    set setName(newName) {
-      this.name = newName;
-    },
+    // set setName(newName) {
+    //   this.name = newName;
+    // },
 
-    set setDescription(newDescription) {
-      this.description = newDescription;
-    },
+    // set setDescription(newDescription) {
+    //   this.description = newDescription;
+    // },
 
-    set setDate(newDate) {
-      this.date = newDate;
-    },
+    // set setDate(newDate) {
+    //   this.date = newDate;
+    // },
 
-    set setPriority(newPriority) {
-      this.priority = newPriority;
-    },
+    // set setPriority(newPriority) {
+    //   this.priority = newPriority;
+    // },
 
-    set setList(newList) {
-      this.name = newList;
-    },
+    // set setList(newList) {
+    //   this.name = newList;
+    // },
   };
+};
+
+const createTask = (chosenList, taskName, description, date, priority) => {
+  const chosenList = getList(chosenList);
+  const newTask = Task(taskName, description, date, priority);
+  chosenList.tasks.push(newTask);
 };
 
 let task1 = Task("Pick up Quentin", "dkdjd", "iddbdbd", "prppr", "Car");
